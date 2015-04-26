73ede64d08aab1ae9c6a79726e7dcd6b38463e2b
aleksanderesk <aleksanderesk@gmail.com>
2015-04-26 11:49:26 -0500

    I'm not a savage.

--- sched_other_rr.c
+++ sched_other_rr.c
@@ -9,21 +9,21 @@
  */
 static void update_curr_other_rr(struct rq *rq)
 {
-	struct task_struct *curr = rq->curr;
-	u64 delta_exec;
+    struct task_struct *curr = rq->curr;
+    u64 delta_exec;
 
-	if (!task_has_other_rr_policy(curr))
-		return;
+    if (!task_has_other_rr_policy(curr))
+        return;
 
-	delta_exec = rq->clock - curr->se.exec_start;
-	if (unlikely((s64)delta_exec < 0))
-		delta_exec = 0;
+    delta_exec = rq->clock - curr->se.exec_start;
+    if (unlikely((s64)delta_exec < 0))
+        delta_exec = 0;
 
-	schedstat_set(curr->se.exec_max, max(curr->se.exec_max, delta_exec));
+    schedstat_set(curr->se.exec_max, max(curr->se.exec_max, delta_exec));
 
-	curr->se.sum_exec_runtime += delta_exec;
-	curr->se.exec_start = rq->clock;
-	cpuacct_charge(curr, delta_exec);
+    curr->se.sum_exec_runtime += delta_exec;
+    curr->se.exec_start = rq->clock;
+    cpuacct_charge(curr, delta_exec);
 }
 
 /*
@@ -31,19 +31,17 @@ static void update_curr_other_rr(struct rq *rq)
  */
 static void enqueue_task_other_rr(struct rq *rq, struct task_struct *p, int wakeup, bool b)
 {
-  update_curr_other_rr(rq);
-  list_add_tail(&p->other_rr_run_list, &rq->other_rr.queue);
-  rq->other_rr.nr_running++;
+    update_curr_other_rr(rq);
+    list_add_tail(&p->other_rr_run_list, &rq->other_rr.queue);
+    rq->other_rr.nr_running++;
 }
 
 static void dequeue_task_other_rr(struct rq *rq, struct task_struct *p, int sleep)
 {
-	// first update the task's runtime statistics
-	update_curr_other_rr(rq);
-  list_del(&p->other_rr_run_list);
-  rq->other_rr.nr_running--;
-
-	// not yet implemented
+    // first update the task's runtime statistics
+    update_curr_other_rr(rq);
+    list_del(&p->other_rr_run_list);
+    rq->other_rr.nr_running--;
 }
 
 /*
@@ -52,16 +50,16 @@ static void dequeue_task_other_rr(struct rq *rq, struct task_struct *p, int slee
  */
 static void requeue_task_other_rr(struct rq *rq, struct task_struct *p)
 {
-	list_move_tail(&p->other_rr_run_list, &rq->other_rr.queue);
+    list_move_tail(&p->other_rr_run_list, &rq->other_rr.queue);
 }
 
 /*
  * current process is relinquishing control of the CPU
  */
-static void
+    static void
 yield_task_other_rr(struct rq *rq)
 {
-	requeue_task_other_rr(rq, &rq->curr);
+    requeue_task_other_rr(rq, &rq->curr);
 }
 
 /*
@@ -77,39 +75,31 @@ static void check_preempt_curr_other_rr(struct rq *rq, struct task_struct *p, in
  */
 static struct task_struct *pick_next_task_other_rr(struct rq *rq)
 {
-	struct task_struct *next;
-	struct list_head *queue = &rq->other_rr.queue;
-	struct other_rr_rq *other_rr_rq = &rq->other_rr;
+    struct task_struct *next;
+    struct list_head *queue = &rq->other_rr.queue;
+    struct other_rr_rq *other_rr_rq = &rq->other_rr;
 
-	if (other_rr_rq->nr_running < 1) {
-    return NULL;
-  }
-  
-  next = list_first_entry(queue, struct task_struct, other_rr_run_list);
-	/* after selecting a task, we need to set a timer to maintain correct
-	 * runtime statistics. You can uncomment this line after you have
-	 * written the code to select the appropriate task.
-	 */
+    //if (other_rr_rq->nr_running < 1) {
+    if (list_empty(queue)) {
+        next = NULL;
+    }
+    else {
+        next = list_first_entry(queue, struct task_struct, other_rr_run_list);
 
-	next->se.exec_start = rq->clock;
-	/* you need to return the selected task here */
-	return next;
+        /* after selecting a task, we need to set a timer to maintain correct
+         * runtime statistics. You can uncomment this line after you have
+         * written the code to select the appropriate task.
+         */
+        next->se.exec_start = rq->clock;
+    }
+    /* you need to return the selected task here */
+    return next;
 }
 
 static void put_prev_task_other_rr(struct rq *rq, struct task_struct *p)
 {
-  /*
-  if (other_rr_time_slice == 0)
-    return;
-
-  if (--p -> task_time_slice == 0) {
-    p -> task_time_slice = other_rr_time_slice;
-    requeue_task_other_rr(rq, p);
-    set_tsk_need_resched(p);
-  }
-  */
-	update_curr_other_rr(rq);
-	p->se.exec_start = 0;
+    update_curr_other_rr(rq);
+    p->se.exec_start = 0;
 }
 
 #ifdef CONFIG_SMP
@@ -122,69 +112,69 @@ static void put_prev_task_other_rr(struct rq *rq, struct task_struct *p)
  */
 static struct task_struct *load_balance_start_other_rr(void *arg)
 {
-	struct rq *rq = arg;
-	struct list_head *head, *curr;
-	struct task_struct *p;
+    struct rq *rq = arg;
+    struct list_head *head, *curr;
+    struct task_struct *p;
 
-	head = &rq->other_rr.queue;
-	curr = head->prev;
+    head = &rq->other_rr.queue;
+    curr = head->prev;
 
-	p = list_entry(curr, struct task_struct, other_rr_run_list);
+    p = list_entry(curr, struct task_struct, other_rr_run_list);
 
-	curr = curr->prev;
+    curr = curr->prev;
 
-	rq->other_rr.other_rr_load_balance_head = head;
-	rq->other_rr.other_rr_load_balance_curr = curr;
+    rq->other_rr.other_rr_load_balance_head = head;
+    rq->other_rr.other_rr_load_balance_curr = curr;
 
-	return p;
+    return p;
 }
 
 static struct task_struct *load_balance_next_other_rr(void *arg)
 {
-	struct rq *rq = arg;
-	struct list_head *curr;
-	struct task_struct *p;
+    struct rq *rq = arg;
+    struct list_head *curr;
+    struct task_struct *p;
 
-	curr = rq->other_rr.other_rr_load_balance_curr;
+    curr = rq->other_rr.other_rr_load_balance_curr;
 
-	p = list_entry(curr, struct task_struct, other_rr_run_list);
-	curr = curr->prev;
-	rq->other_rr.other_rr_load_balance_curr = curr;
+    p = list_entry(curr, struct task_struct, other_rr_run_list);
+    curr = curr->prev;
+    rq->other_rr.other_rr_load_balance_curr = curr;
 
-	return p;
+    return p;
 }
 
-static unsigned long
+    static unsigned long
 load_balance_other_rr(struct rq *this_rq, int this_cpu, struct rq *busiest,
-		unsigned long max_load_move,
-		struct sched_domain *sd, enum cpu_idle_type idle,
-		int *all_pinned, int *this_best_prio)
+        unsigned long max_load_move,
+        struct sched_domain *sd, enum cpu_idle_type idle,
+        int *all_pinned, int *this_best_prio)
 {
-	struct rq_iterator other_rr_rq_iterator;
+    struct rq_iterator other_rr_rq_iterator;
 
-	other_rr_rq_iterator.start = load_balance_start_other_rr;
-	other_rr_rq_iterator.next = load_balance_next_other_rr;
-	/* pass 'busiest' rq argument into
-	 * load_balance_[start|next]_other_rr iterators
-	 */
-	other_rr_rq_iterator.arg = busiest;
+    other_rr_rq_iterator.start = load_balance_start_other_rr;
+    other_rr_rq_iterator.next = load_balance_next_other_rr;
+    /* pass 'busiest' rq argument into
+     * load_balance_[start|next]_other_rr iterators
+     */
+    other_rr_rq_iterator.arg = busiest;
 
-	return balance_tasks(this_rq, this_cpu, busiest, max_load_move, sd,
-			     idle, all_pinned, this_best_prio, &other_rr_rq_iterator);
+    return balance_tasks(this_rq, this_cpu, busiest, max_load_move, sd,
+            idle, all_pinned, this_best_prio, &other_rr_rq_iterator);
 }
 
-static int
+    static int
 move_one_task_other_rr(struct rq *this_rq, int this_cpu, struct rq *busiest,
-		 struct sched_domain *sd, enum cpu_idle_type idle)
+        struct sched_domain *sd, enum cpu_idle_type idle)
 {
-	struct rq_iterator other_rr_rq_iterator;
+    struct rq_iterator other_rr_rq_iterator;
 
-	other_rr_rq_iterator.start = load_balance_start_other_rr;
-	other_rr_rq_iterator.next = load_balance_next_other_rr;
-	other_rr_rq_iterator.arg = busiest;
+    other_rr_rq_iterator.start = load_balance_start_other_rr;
+    other_rr_rq_iterator.next = load_balance_next_other_rr;
+    other_rr_rq_iterator.arg = busiest;
 
-	return iter_move_one_task(this_rq, this_cpu, busiest, sd, idle,
-				  &other_rr_rq_iterator);
+    return iter_move_one_task(this_rq, this_cpu, busiest, sd, idle,
+            &other_rr_rq_iterator);
 }
 #endif
 
@@ -193,21 +183,33 @@ move_one_task_other_rr(struct rq *this_rq, int this_cpu, struct rq *busiest,
  */
 static void task_tick_other_rr(struct rq *rq, struct task_struct *p,int queued)
 {
-	// first update the task's runtime statistics
-	update_curr_other_rr(rq);
+    // first update the task's runtime statistics
+    update_curr_other_rr(rq);
 
-	// not yet implemented
-  if (other_rr_time_slice == 0) {
-    return;
-  } else {
-    if (p->task_time_slice != 0) { 
-      p->task_time_slice--;
-    } else {
-        p->task_time_slice = other_rr_time_slice;
-        set_tsk_need_resched(p);
-        requeue_task_other_rr(rq, p);
-    }
-  } 
+    // not yet implemented
+    if (other_rr_time_slice == 0) {
+        return;
+    } 
+    else {
+        if (p->task_time_slice == 0) {
+            p->task_time_slice = other_rr_time_slice;
+            set_tsk_need_resched(p);
+            requeue_task_other_rr(rq, p);
+        }
+        else {
+            p->task_time_slice--;
+        }
+        /*
+        if (p->task_time_slice != 0) { 
+            p->task_time_slice--;
+        } 
+        else {
+            p->task_time_slice = other_rr_time_slice;
+            set_tsk_need_resched(p);
+            requeue_task_other_rr(rq, p);
+        }
+        */
+    } 
 }
 
 /*
@@ -216,54 +218,54 @@ static void task_tick_other_rr(struct rq *rq, struct task_struct *p,int queued)
  */
 static void set_curr_task_other_rr(struct rq *rq)
 {
-	struct task_struct *p = rq->curr;
-	p->se.exec_start = rq->clock;
+    struct task_struct *p = rq->curr;
+    p->se.exec_start = rq->clock;
 }
 
 /*
  * We switched to the sched_other_rr class.
  */
 static void switched_to_other_rr(struct rq *rq, struct task_struct *p,
-			     int running)
+        int running)
 {
-	/*
-	 * Kick off the schedule if running, otherwise just see
-	 * if we can still preempt the current task.
-	 */
-	if (running)
-		resched_task(rq->curr);
-	else
-		check_preempt_curr(rq, p, 0);
+    /*
+     * Kick off the schedule if running, otherwise just see
+     * if we can still preempt the current task.
+     */
+    if (running)
+        resched_task(rq->curr);
+    else
+        check_preempt_curr(rq, p, 0);
 }
 
-static int
+    static int
 select_task_rq_other_rr(struct rq *rq, struct task_struct *p, int sd_flag, int flags)
 {
-	if (sd_flag != SD_BALANCE_WAKE)
-		return smp_processor_id();
+    if (sd_flag != SD_BALANCE_WAKE)
+        return smp_processor_id();
 
-	return task_cpu(p);
+    return task_cpu(p);
 }
 
 const struct sched_class other_rr_sched_class = {
-	.next			= &idle_sched_class,
-	.enqueue_task		= enqueue_task_other_rr,
-	.dequeue_task		= dequeue_task_other_rr,
-	.yield_task		= yield_task_other_rr,
+    .next			= &idle_sched_class,
+    .enqueue_task		= enqueue_task_other_rr,
+    .dequeue_task		= dequeue_task_other_rr,
+    .yield_task		= yield_task_other_rr,
 
-	.check_preempt_curr	= check_preempt_curr_other_rr,
+    .check_preempt_curr	= check_preempt_curr_other_rr,
 
-	.pick_next_task		= pick_next_task_other_rr,
-	.put_prev_task		= put_prev_task_other_rr,
+    .pick_next_task		= pick_next_task_other_rr,
+    .put_prev_task		= put_prev_task_other_rr,
 
 #ifdef CONFIG_SMP
-	.load_balance		= load_balance_other_rr,
-	.move_one_task		= move_one_task_other_rr,
+    .load_balance		= load_balance_other_rr,
+    .move_one_task		= move_one_task_other_rr,
 #endif
 
-	.switched_to  = switched_to_other_rr,
-	.select_task_rq = select_task_rq_other_rr,
+    .switched_to  = switched_to_other_rr,
+    .select_task_rq = select_task_rq_other_rr,
 
-	.set_curr_task          = set_curr_task_other_rr,
-	.task_tick		= (void *)task_tick_other_rr,
+    .set_curr_task          = set_curr_task_other_rr,
+    .task_tick		= (void *)task_tick_other_rr,
 };
